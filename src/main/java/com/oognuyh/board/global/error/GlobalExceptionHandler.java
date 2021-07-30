53245fe9c9e8f1d677fe5e3f007168e43cc1fdf4
Hyungoo Kang <oognuyh@gmail.com>
2021-07-30 20:30:42 +0900

    feat: add access denied exception

--- src/main/java/com/oognuyh/board/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/oognuyh/board/global/error/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import com.oognuyh.board.post.web.exception.PostDoesNotExistException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -51,8 +52,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e) {
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e) {
+        e.printStackTrace();
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
